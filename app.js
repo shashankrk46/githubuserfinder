8bfe5c06ccf565f3d53cfe13e8b2ed45d7c31065
shashankrk46 <shashankrk46@gmail.com>
2020-02-26 19:18:58 +0530

    data displayed on UI

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-// search input
-const searchUser=document.getElementById('searchUser');
 
 // initiate github
 const github=new Github;
+
+// init UI
+const ui=new UI;
+
+// search input
+
+const searchUser=document.getElementById('searchUser');
+
+
 // search input event listener
 searchUser.addEventListener('keyup',(e)=>{
 //   get input text
@@ -12,11 +19,12 @@ if(userText !==''){
 //   make http call
 github.getUser(userText)
 .then(data=>{
-    if(data.profile.message='not found'){
+    if(data.profile.message==='not found'){
         // show alert
 
     }else{
         // show profile
+        ui.showProfile(data.profile);
 
     }
 })
--- github.js
+++ github.js
@@ -1,12 +1,15 @@
 class Github{
     constructor(){
-        this.client_id='834bf6e68b47e02fdcd3';
-        this.client_secret='88c334dc90448754f155a13f62383f1c165ea611';
+        this.config = {
+            headers: {
+              Authorization: '399edc405afa9db0ababe6311ec3c5040607d308 '
+            }
+        }
     }
 
     async getUser(user){
-        const profileResponse=await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}
-        &client_secret=${this.client_secret}`);
+        const profileResponse=await fetch(`https://api.github.com/users/${user}`,
+        this.config );
 
         const profile=await profileResponse.json();
 
