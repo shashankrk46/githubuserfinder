e1235acadd07ff5e728a7eda782f4a6e5b5626d3
shashankrk46 <shashankrk46@gmail.com>
2020-02-28 16:22:47 +0530

    fixed few errors

--- github.js
+++ github.js
@@ -17,7 +17,7 @@ class Github{
 
         const profile=await profileResponse.json();
 
-        const repos=await reposResponse.json();
+        const repos=await repoResponse.json();
 
         return{
           profile,
